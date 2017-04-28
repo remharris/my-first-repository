50bf7a4ea3318962332892d9fe920c07dcc0cb13
Matthew F. Short <short.matthew.f@gmail.com>
2017-04-27 21:11:45 -0400

    now with functions

--- examples/js/02/app.js
+++ examples/js/02/app.js
@@ -4,15 +4,18 @@ const myElements = [
     content: "This is the first element on the page.",
     fontSize: "22px"
   },
+
   {
     tagName: "p",
     content: "This is the second element on the page, it's a paragraph...",
     fontSize: "14px"
   },
+
   {
     tagName: "img",
     src: "https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg"
   },
+
   {
     tagName: "p",
     content: "Oh, I decided to add more...",
@@ -24,14 +27,20 @@ let body = document.querySelector('body');
 
 for (let i = 0; i < myElements.length; i = i + 1) {
   let currentElement = myElements[i];
-  let newNode = document.createElement(currentElement.tagName);
+  let currentNewNode = makeMyNode(currentElement);
+  
+  body.appendChild(currentNewNode);
+}
 
-  if (currentElement.tagName == "img") {
-    newNode.src = currentElement.src;
+function makeMyNode(nodeData) {
+  let newNode = document.createElement(nodeData.tagName);
+
+  if (nodeData.tagName == "img") {
+    newNode.src = nodeData.src;
   } else {
-    newNode.innerText = currentElement.content;
-    newNode.style.fontSize = currentElement.fontSize;
+    newNode.innerText = nodeData.content;
+    newNode.style.fontSize = nodeData.fontSize;
   }
 
-  body.appendChild(newNode);
+  return newNode;
 }
